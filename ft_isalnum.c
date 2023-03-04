1d5ffaebc64e3aa7b704dec11fedfbfa1bb5071d
Sasin Phimsiri <sasinp@arv.co.th>
2023-03-04 15:51:56 +0700

    Added ft_isalnum _isalpha _isascii _isdigit _isprint

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 10:57:27 by sphimsir          #+#    #+#             */
-/*   Updated: 2023/03/04 15:44:22 by sphimsir         ###   ########.fr       */
+/*   Created: 2023/03/04 15:43:49 by sphimsir          #+#    #+#             */
+/*   Updated: 2023/03/04 15:43:57 by sphimsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-typedef struct s_list
+int	ft_isalnum(int c)
 {
-	void			*content;
-	struct s_list	*next;
-}					t_list;
-
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_isalnum(int c);
-
-#endif
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	return (0);
+}
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 10:57:27 by sphimsir          #+#    #+#             */
-/*   Updated: 2023/03/04 15:44:22 by sphimsir         ###   ########.fr       */
+/*   Created: 2023/03/04 15:25:43 by sphimsir          #+#    #+#             */
+/*   Updated: 2023/03/04 15:29:08 by sphimsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-typedef struct s_list
+int	ft_isalpha(int c)
 {
-	void			*content;
-	struct s_list	*next;
-}					t_list;
-
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_isalnum(int c);
-
-#endif
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
