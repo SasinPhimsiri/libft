908e9cefbb2eaed97d5b959ede4d2c1132ce377e
Sasin Phimsiri <sasinp@arv.co.th>
2023-03-04 21:34:51 +0700

    Added ft_strlcat _strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 16:00:14 by sphimsir          #+#    #+#             */
+/*   Updated: 2023/03/04 16:19:36 by sphimsir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	c;
+
+	if (src == NULL)
+		return ((size_t) NULL);
+	i = 0;
+	c = 0;
+	while (src[c])
+		c++;
+	if (dstsize < 1)
+		return (c);
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (c);
+}
