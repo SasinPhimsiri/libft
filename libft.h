bdd89830cdcfafee9eb2aa23b6ee53238bdc3435
Sasin Phimsiri <sasinp@arv.co.th>
2023-03-06 15:10:07 +0700

    Modified libft.h

--- libft.h
+++ libft.h
@@ -6,23 +6,23 @@
 /*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 10:57:27 by sphimsir          #+#    #+#             */
-/*   Updated: 2023/03/06 14:50:36 by sphimsir         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:07:57 by sphimsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#define SIZE_MAX          UINTPTR_MAX
-#if __LP64__
-#define __WORDSIZE 64
-#else
-#define __WORDSIZE 32
-#endif
-#if __WORDSIZE == 64
-#define UINTPTR_MAX       18446744073709551615UL
-#else
-#define UINTPTR_MAX       4294967295UL
-#endif
+// #define SIZE_MAX          UINTPTR_MAX
+// #if __LP64__
+// #define __WORDSIZE 64
+// #else
+// #define __WORDSIZE 32
+// #endif
+// #if __WORDSIZE == 64
+// #define UINTPTR_MAX       18446744073709551615UL
+// #else
+// #define UINTPTR_MAX       4294967295UL
+// #endif
 # include <stdio.h>
 # include <unistd.h>
 # include <stddef.h>
