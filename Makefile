e7ef6ec75056d17d8142d05106b7459334829fab
Sasin Phimsiri <sasinp@arv.co.th>
2023-03-06 14:36:29 +0700

    Merged: Added ft_bzero & Modified ft_calloc

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/03 11:09:33 by sphimsir          #+#    #+#              #
-#    Updated: 2023/03/06 10:19:30 by sphimsir         ###   ########.fr        #
+#    Updated: 2023/03/06 14:15:33 by sphimsir         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,7 +30,9 @@ FILES = ft_strlen \
 		ft_isalnum \
 		ft_calloc \
 		ft_strchr \
-		ft_strrchr
+		ft_strrchr \
+		ft_bzero
+
 
 SRCS_DIR = ./
 SRCS = $(addprefix $(SRCS_DIR), $(addsuffix .c, $(FILES)))
--- ft_bzero.c
+++ ft_bzero.c
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/05 22:09:54 by sphimsir          #+#    #+#             */
-/*   Updated: 2023/03/06 14:17:19 by sphimsir         ###   ########.fr       */
+/*   Created: 2023/03/06 14:13:54 by sphimsir          #+#    #+#             */
+/*   Updated: 2023/03/06 14:23:14 by sphimsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	ft_bzero(void *s, size_t n)
 {
-	char	*m;
-	size_t	i;
+	unsigned int	i;
+	char			*b;
 
-	if (count == SIZE_MAX || size == SIZE_MAX)
-		return (NULL);
-	m = malloc(count * size);
-	if (m == NULL)
-		return (NULL);
 	i = 0;
-	while (i < count * size)
-	{
-		m[i] = 0;
-		i++;
-	}
-	return (m);
+	b = s;
+	while (i < n)
+		b[i++] = '\0';
 }
