fb3cd4ebc95c85e3f40e18bbb7fff25f904733c1
Sasin Phimsiri <sasinp@arv.co.th>
2023-04-02 11:06:48 +0700

    Fixed ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:16:25 by sphimsir          #+#    #+#             */
-/*   Updated: 2023/03/06 15:37:28 by sphimsir         ###   ########.fr       */
+/*   Updated: 2023/04/02 11:05:55 by sphimsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (n == 0)
-		return (0);
-	while (n-- && (*s1 != '\0' || *s2 != '\0'))
-	{
+	while (n-- > 0 && (*s1 || *s2))
 		if (*s1++ != *s2++)
 			return (*(unsigned char *)(s1 - 1) - *(unsigned char *)(s2 - 1));
-	}
 	return (0);
 }
