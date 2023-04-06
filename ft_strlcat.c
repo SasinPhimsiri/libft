43433891720ebd29f98ee5af72a8f25b47529428
Sasin Phimsiri <sasinp@arv.co.th>
2023-04-06 11:23:30 +0700

    Swaped condition before concat loop

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sphimsir <sphimsir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 16:00:10 by sphimsir          #+#    #+#             */
-/*   Updated: 2023/03/05 14:21:35 by sphimsir         ###   ########.fr       */
+/*   Updated: 2023/04/06 11:21:24 by sphimsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	len_d = ft_strlen(dst);
 	i = ft_strlen(dst);
 	j = 0;
-	while (src[j] && i < dstsize - 1)
-		dst[i++] = src[j++];
 	if (dstsize < len_d)
 		return (ft_strlen(src) + dstsize);
+	while (src[j] && i < dstsize - 1)
+		dst[i++] = src[j++];
 	dst[i] = '\0';
 	return (len_d + ft_strlen(src));
 }
